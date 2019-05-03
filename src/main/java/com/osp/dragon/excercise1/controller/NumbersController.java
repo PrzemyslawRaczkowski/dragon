a4ad1df2a6313a5cf5a6de88bfb7574db2e7e1b6
Raczk0wski <36660255+Raczk0wski@users.noreply.github.com>
2019-05-03 17:06:59 +0200

    Added random generate numbers method

--- src/main/java/com/osp/dragon/excercise1/controller/NumbersController.java
+++ src/main/java/com/osp/dragon/excercise1/controller/NumbersController.java
@@ -6,8 +6,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import java.util.Random;
 
 @RestController
 @RequestMapping(value = "/numbers")
@@ -19,8 +21,15 @@ public class NumbersController {
 
     @GetMapping
     @ResponseBody
-    public List<Integer> getAllArticles() {
-        return Collections.emptyList();
+    public List<Integer> generateRandomNumbers() {
+        Random random = new Random();
+        ArrayList<Integer> ListOfNumbers = new ArrayList<>(100);
+
+        for (int i = 0; i < 100; i++) {
+            int n = random.nextInt(101);
+            ListOfNumbers.add(n);
+        }
+        return ListOfNumbers;
     }
 
 
