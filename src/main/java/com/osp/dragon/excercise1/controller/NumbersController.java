66ad381fcb8713ce31ffe7fa806e657d7bf32829
raczkowski <Reczk000wsk!!!pppp>
2019-05-03 19:33:59 +0200

    DRAGON: Generator added

--- src/main/java/com/osp/dragon/excercise1/controller/NumbersController.java
+++ src/main/java/com/osp/dragon/excercise1/controller/NumbersController.java
@@ -1,37 +1,26 @@
 package com.osp.dragon.excercise1.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
-import java.util.Random;
 
 @RestController
 @RequestMapping(value = "/numbers")
 public class NumbersController {
 
-    @Autowired
-    public NumbersController() {
+    private final RandomNumbersGenerator generator;
+
+    public NumbersController(RandomNumbersGenerator generator) {
+        this.generator = generator;
     }
 
     @GetMapping
     @ResponseBody
-    public List<Integer> generateRandomNumbers() {
-        Random random = new Random();
-        List<Integer> ListOfNumbers = new ArrayList<>(100);
-
-        for (int i = 0; i < 100; i++) {
-            int n = random.nextInt(101);
-            ListOfNumbers.add(n);
-        }
-
-        return ListOfNumbers;
+    public List<Integer> getRandomNumbers() {
+        return generator.generate();
     }
-
 
 }
