33f506c730f7f38f0d5a10dc2693a4d353a0d965
raczkowski <Reczk000wsk!!!pppp>
2019-05-04 20:06:34 +0200

    DRAGON: Test added

---
+++ src/test/com/osp/dragon/excercise1/controller/RandomNumbersGeneratorTest.java
@@ -0,0 +1,24 @@
+package com.osp.dragon.excercise1.controller;
+
+import org.junit.Test;
+
+import java.util.List;
+
+import static org.junit.Assert.*;
+
+public class RandomNumbersGeneratorTest {
+
+    @Test
+    public void should_return_proper_number_of_samples() {
+        // given:
+        int numberOfSamples = 100;
+        RandomNumbersGenerator generator = new RandomNumbersGenerator();
+
+        // when:
+        List<Integer> result = generator.generateRandomNumbersFromRange(10, 100, numberOfSamples);
+
+        // then:
+        assertEquals(result.size(), numberOfSamples);
+    }
+
+}
