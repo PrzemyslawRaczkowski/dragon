b148d451d209e9486e08b69604aee6a202fcdf7f
raczkowski <Reczk000wsk!!!pppp>
2019-05-04 19:53:09 +0200

    DRAGON: Refactoring code 2

--- src/main/java/com/osp/dragon/excercise1/controller/RandomNumbersGenerator.java
+++ src/main/java/com/osp/dragon/excercise1/controller/RandomNumbersGenerator.java
@@ -9,20 +9,17 @@ import java.util.Random;
 @Component
 public class RandomNumbersGenerator {
 
-        public int generate(int min, int max) {
-
-            Random random = new Random();
-
-            return random.nextInt(max - min + 1) + min;
-        }
-
-        public ArrayList<Integer> generateRandomNumbersOfSections(int minimum, int maximum, int numberOfSamples) {
-
-            ArrayList<Integer> List = new ArrayList<>();
-
-            for (int i = 0; i <= numberOfSamples; i++) {
-                generate(minimum, maximum);
-            }
-            return List;
-        }
+    public int generateRandomNumbersFromRange(int min, int max) {
+        Random random = new Random();
+        return random.nextInt(max - min + 1) + min;
     }
+
+    public List<Integer> generateRandomNumbersFromRange(int minimum, int maximum, int numberOfSamples) {
+        List<Integer> numbers = new ArrayList<>();
+        for (int i = 0; i <= numberOfSamples; i++) {
+            numbers.add(generateRandomNumbersFromRange(minimum, maximum));
+        }
+
+        return numbers;
+    }
+}
