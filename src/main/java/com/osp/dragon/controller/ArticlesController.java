488378725a662a4132a8168c1bb68f283a731549
raczkowski <Reczk000wsk!!!pppp>
2019-09-25 19:48:07 +0200

    DRAGON: Refactoring

---
+++ src/main/java/com/osp/dragon/controller/ArticlesController.java
@@ -0,0 +1,33 @@
+package com.osp.dragon.controller;
+
+import com.osp.dragon.model.Article;
+import com.osp.dragon.service.ArticleService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(value = "/articles")
+public class ArticlesController {
+
+    private ArticleService articleService;
+
+    @Autowired
+    public ArticlesController(ArticleService articleService) {
+        this.articleService = articleService;
+    }
+
+    @GetMapping
+    @ResponseBody
+    public List<Article> getAllArticles() {
+        return articleService.getAllArticles();
+    }
+
+    @GetMapping(value = "/{id}")
+    @ResponseBody
+    public Article getArticle(@PathVariable String id) {
+        return articleService.getArticleById(id);
+    }
+
+}
---
+++ src/main/java/com/osp/dragon/service/ArticleService.java
@@ -0,0 +1,47 @@
+package com.osp.dragon.service;
+
+import com.osp.dragon.model.Article;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@Service
+public class ArticleService {
+
+    private final Map<String, Article> articlesMap;
+
+    public ArticleService() {
+        this.articlesMap = initializeArticlesCache();
+    }
+
+    public Article getArticleById(String id) {
+        return articlesMap.get(id);
+    }
+
+    public List<Article> getAllArticles() {
+        return new ArrayList<>(articlesMap.values());
+    }
+
+    private Map<String, Article> initializeArticlesCache() {
+        Map<String, Article> map = new HashMap<>();
+
+        Article article1 = new Article(12345, "Title", "First Article", "raczkowski", "10/12/2018");
+        Article article2 = new Article(12345, "Title", "First Article", "raczkowski", "10/12/2018");
+        Article article3 = new Article(12345, "Title", "First Article", "raczkowski", "10/12/2018");
+        Article article4 = new Article(12345, "Title", "First Article", "raczkowski", "10/12/2018");
+        Article article5 = new Article(12345, "Title", "First Article", "raczkowski", "10/12/2018");
+        Article article6 = new Article(12345, "Title", "First Article", "raczkowski", "10/12/2018");
+
+        map.put("1", article1);
+        map.put("2", article2);
+        map.put("3", article3);
+        map.put("4", article4);
+        map.put("5", article5);
+        map.put("6", article6);
+
+        return map;
+    }
+}
