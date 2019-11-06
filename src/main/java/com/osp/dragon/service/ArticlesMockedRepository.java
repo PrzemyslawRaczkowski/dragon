18ea31a92ea300755b7ee74dbded45526f3fd2d8
p.raczkowski <p.raczkowski@ocado.com>
2019-11-06 22:03:31 +0100

    DRAGON: Tests added

--- src/main/java/com/osp/dragon/service/ArticlesMockedRepository.java
+++ src/main/java/com/osp/dragon/service/ArticlesMockedRepository.java
@@ -1,11 +1,21 @@
 package com.osp.dragon.service;
 
+import static java.time.LocalDate.now;
+import static java.util.stream.Collectors.toMap;
+import static java.util.stream.Stream.of;
+
 import com.osp.dragon.model.Article;
+
 import org.springframework.stereotype.Service;
 
+import java.time.Instant;
+import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 @Service
 public class ArticlesMockedRepository {
@@ -21,11 +31,18 @@ public class ArticlesMockedRepository {
     }
 
     public List<Article> getAllArticles() {
-        return (List<Article>) articles.values();
+        return new ArrayList<>(articles.values());
     }
 
     private Map<Integer, Article> loadArticles() {
-        return new HashMap<>();
+        return of(
+                new Article(1, "Title_1", "Content_1", "Author_1", now()),
+                new Article(2, "Title_2", "Content_2", "Author_2", now()),
+                new Article(3, "Title_3", "Content_3", "Author_3", now()),
+                new Article(4, "Title_4", "Content_4", "Author_4", now()),
+                new Article(5, "Title_5", "Content_5", "Author_5", now()),
+                new Article(6, "Title_6", "Content_6", "Author_6", now())
+        ).collect(toMap(Article::getId, article -> article));
     }
 
 }
--- src/test/com/osp/dragon/service/ArticleServiceTest.java
+++ src/test/com/osp/dragon/service/ArticleServiceTest.java
@@ -1,13 +1,63 @@
 package com.osp.dragon.service;
 
+import static java.time.LocalDate.now;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
+import static org.assertj.core.api.Assertions.catchThrowable;
+
+import java.util.List;
+
+import org.junit.Before;
 import org.junit.Test;
 
+import com.osp.dragon.exception.ArticleNotFoundException;
+import com.osp.dragon.model.Article;
+
 public class ArticleServiceTest {
+    private ArticleService service;
+
+    @Before
+    public void setUp() {
+        service = new ArticleService(new ArticlesMockedRepository());
+    }
 
     @Test
     public void should_return_article_by_id() {
         // given:
+        int id = 1;
+        Article expectedResult = new Article(1, "Title_1", "Content_1", "Author_1", now());
 
         // when:
+        Article result = service.getArticleById(id);
+
+        // then:
+        assertThat(result).isNotNull();
+        assertThat(result).isEqualToComparingFieldByField(expectedResult);
+    }
+
+    @Test
+    public void should_throw_exception_when_article_not_found_by_id() {
+        // given:
+        int id = 1000;
+        String expectedMessage = "Article with id: 1000 has not been found.";
+
+        // when:
+        Throwable throwable = catchThrowable(() -> service.getArticleById(id));
+
+        // then:
+        assertThat(throwable).isInstanceOf(ArticleNotFoundException.class);
+        assertThat(throwable.getMessage()).isEqualTo(expectedMessage);
+    }
+
+    @Test
+    public void should_return_all_articles() {
+        // given:
+        int expectedResult = 6;
+        // when:
+        List<Article> articles = service.getAllArticles();
+
+        // then:
+        assertThat(articles).hasSize(expectedResult);
+
     }
 }
